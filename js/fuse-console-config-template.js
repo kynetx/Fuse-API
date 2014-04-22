93628dace8a092eeb6608cb5a0bbec4e643f6f12
windley <pjw@kynetx.com>
2014-04-21 21:24:33 -0600

    A few template updates

---
+++ js/fuse-console-config-template.js
@@ -0,0 +1,8 @@
+var username = "user@example.com";
+var password = "my awesome password"; 
+var test_device = {apiKey: "<carvoyant API key>", 
+		   secToken: "<carvoyant secret token>", 
+		   deviceId: "<carvoyant device ID>"};
+
+
+
